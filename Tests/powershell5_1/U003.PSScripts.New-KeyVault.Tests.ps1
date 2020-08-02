39e8f4f494c3feecf6a994bdc46fafa0fc40122f
olusola-adio <olusola_adio@hotmail.com>
2020-08-03 00:55:52 +0100

    test

--- Tests/powershell5_1/U003.PSScripts.New-KeyVault.Tests.ps1
+++ Tests/powershell5_1/U003.PSScripts.New-KeyVault.Tests.ps1
@@ -2,12 +2,11 @@ Push-Location -Path $PSScriptRoot\..\..\PSScripts\
 
 Describe "New-KeyVault unit tests" -Tag "Unit" {
 
-    Mock Get-AzureRmResourceGroup { return ConvertFrom-Json '{ "ResourceGroupName": "mon-foobar-rg", "Location": "northeurope" }' }
-    Mock New-AzureRmKeyVault { return ConvertFrom-Json '{ "VaultName": "mon-foobar-kv", "AccessPolicies": [ { "ObjectId": "12345678-abcd-1234-5678-1234567890ab" } ] }' }
-    Mock Remove-AzureRmKeyVaultAccessPolicy
-
-    $kvname = "mon-foobar-kv"
-    $rgname = "mon-foobar-rg"
+    BeforeAll{
+        Mock Get-AzureRmResourceGroup { return ConvertFrom-Json '{ "ResourceGroupName": "mon-foobar-rg", "Location": "northeurope" }' }
+        Mock New-AzureRmKeyVault { return ConvertFrom-Json '{ "VaultName": "mon-foobar-kv", "AccessPolicies": [ { "ObjectId": "12345678-abcd-1234-5678-1234567890ab" } ] }' }
+        Mock Remove-AzureRmKeyVaultAccessPolicy
+    }
 
     It "Should create a key vault if one does not exist" {
         Mock Get-AzureRmKeyVault { return $null }
@@ -24,7 +23,7 @@ Describe "New-KeyVault unit tests" -Tag "Unit" {
         Mock Get-AzureRmKeyVault { return ConvertFrom-Json '{ "VaultName": "mon-foobar-kv", "ResourceGroupName": "mon-foobar-rg", "Location": "northeurope" }' }
 
         .\..\..\PSScripts\New-KeyVault -KeyVaultName "mon-foobar-kv" -ResourceGroupName "mon-foobar-rg"
-        
+
         Should -Invoke Get-AzureRmKeyVault -Exactly 1 -Scope It
         Should -Invoke Get-AzureRmResourceGroup -Exactly 0 -Scope It
         Should -Invoke New-AzureRmKeyVault -Exactly 0 -Scope It
