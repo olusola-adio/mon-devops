154c44802c6098be86bf4fceeecc3d6615fffa34
olusola-adio <olusola_adio@hotmail.com>
2020-08-02 19:54:57 +0100

    added servicebus

---
+++ Tests/arm/A020.ArmTemplates.ServiceBusTopic.Tests.ps1
@@ -0,0 +1,28 @@
+# common variables
+$ResourceGroupName = "mon-test-template-rg"
+$TemplateFile = "$PSScriptRoot\..\..\ArmTemplates\ServiceBus\servicebus-topic.json"
+
+Describe "Service Bus Topic Deployment Tests" -Tag "Acceptance" {
+  
+  Context "When deploying the Service Bus Topic" {
+    $TemplateParameters = @{
+      serviceBusNamespaceName = "mon-foo-bar-ns"
+      serviceBusTopicName     = "topic-name"
+    }
+    $TestTemplateParams = @{
+      ResourceGroupName       = $ResourceGroupName
+      TemplateFile            = $TemplateFile
+      TemplateParameterObject = $TemplateParameters
+    }
+  
+    It "Should be deployed successfully" {
+      $output = Test-AzureRmResourceGroupDeployment @TestTemplateParams
+      $output | Should -Be $null
+    }
+
+    if ($output) {
+      Write-Error $output.Message
+    }
+
+  }
+}
