80274220bf0ff0c5a6cdd76c19a1d8c88c9c32ed
olusola-adio <olusola_adio@hotmail.com>
2020-08-02 22:02:09 +0100

    quality

--- Tests/Invoke-QualityTests.ps1
+++ Tests/Invoke-QualityTests.ps1
@@ -17,23 +17,23 @@ Invoke-QualityTests.ps1
 param()
 
 
-# Install-Module -Name Pester -RequiredVersion 4.10.1 -Force -SkipPublisherCheck
+Install-Module -Name Pester -RequiredVersion 4.10.1 -Force -SkipPublisherCheck
 
-# Import-Module -Name Pester -Scope Local
+Import-Module -Name Pester -Scope Local
 
-# $pesterModules = @( Get-Module -Name "Pester" -ErrorAction "SilentlyContinue" );
-# if( ($null -eq $pesterModules) -or ($pesterModules.Length -eq 0) )
-# {
-#     throw "no pester module loaded!";
-# }
-# if( $pesterModules.Length -gt 1 )
-# {
-#     throw "multiple pester modules loaded!";
-# }
-# if( $pesterModules[0].Version -ne ([version] "4.10.1") )
-# {
-#     throw "unsupported pester version '$($pesterModules[0].Version)'";
-# }
+$pesterModules = @( Get-Module -Name "Pester" -ErrorAction "SilentlyContinue" );
+if( ($null -eq $pesterModules) -or ($pesterModules.Length -eq 0) )
+{
+    throw "no pester module loaded!";
+}
+if( $pesterModules.Length -gt 1 )
+{
+    throw "multiple pester modules loaded!";
+}
+if( $pesterModules[0].Version -ne ([version] "4.10.1") )
+{
+    throw "unsupported pester version '$($pesterModules[0].Version)'";
+}
 
 $TestParameters = @{
     OutputFormat = 'NUnitXml'
@@ -44,11 +44,11 @@ $TestParameters = @{
 }
 
 # Invoke tests
-#$Result = Invoke-Pester @TestParameters
+$Result = Invoke-Pester @TestParameters
 
 
 
-$Result = Invoke-Pester -Script "$PSScriptRoot\Quality" -PassThru -Verbose -OutputFile "$PSScriptRoot\TEST-Quality.xml" -OutputFormat NUnitXml -Show All
+#$Result = Invoke-Pester -Script "$PSScriptRoot\Quality" -PassThru -Verbose -OutputFile "$PSScriptRoot\TEST-Quality.xml" -OutputFormat NUnitXml -Show All
 
 # report failures
 if ($Result.FailedCount -ne 0) { 
