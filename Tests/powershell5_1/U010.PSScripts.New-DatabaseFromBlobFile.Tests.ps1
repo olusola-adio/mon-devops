3f56409ab59a3631cd5d8d1f9d942761e5484cee
olusola-adio <olusola_adio@hotmail.com>
2020-08-03 09:28:31 +0100

    tests

--- Tests/powershell5_1/U010.PSScripts.New-DatabaseFromBlobFile.Tests.ps1
+++ Tests/powershell5_1/U010.PSScripts.New-DatabaseFromBlobFile.Tests.ps1
@@ -18,29 +18,32 @@ function Get-AzureRmSqlDatabase {}
 
 Describe "New-DatabaseFromBlobFile unit tests" -Tag "Unit" {
 
-    Mock New-AzureRmSqlDatabaseImport { return ConvertFrom-Json '{ "OperationStatusLink": "https://management.azure.com/subscriptions/blah/guid?apiversion=1-2-3" }' }
-    Mock Get-AzureRmSqlDatabaseImportExportStatus { return ConvertFrom-Json '{ "Status": "Succeeded", "StatusMessage": "" }' }
-    Mock Set-AzureRmSqlDatabase
-    Mock Get-AzureRmSqlDatabase
+    BeforeAll {
+
+        Mock New-AzureRmSqlDatabaseImport { return ConvertFrom-Json '{ "OperationStatusLink": "https://management.azure.com/subscriptions/blah/guid?apiversion=1-2-3" }' }
+        Mock Get-AzureRmSqlDatabaseImportExportStatus { return ConvertFrom-Json '{ "Status": "Succeeded", "StatusMessage": "" }' }
+        Mock Set-AzureRmSqlDatabase
+        Mock Get-AzureRmSqlDatabase
+    }
 
     It "Should create a database" {
-        .\New-DatabaseFromBlobFile @params
+        .\..\..\PSScripts\New-DatabaseFromBlobFile @params
 
-        Assert-MockCalled New-AzureRmSqlDatabaseImport -Exactly 1 -Scope It
-        Assert-MockCalled Get-AzureRmSqlDatabaseImportExportStatus -Exactly 1 -Scope It
-        Assert-MockCalled Set-AzureRmSqlDatabase -Exactly 0 -Scope It
-        Assert-MockCalled Get-AzureRmSqlDatabase -Exactly 1 -Scope It
+        Should -Invoke  New-AzureRmSqlDatabaseImport -Exactly 1 -Scope It
+        Should -Invoke  Get-AzureRmSqlDatabaseImportExportStatus -Exactly 1 -Scope It
+        Should -Invoke  Set-AzureRmSqlDatabase -Exactly 0 -Scope It
+        Should -Invoke  Get-AzureRmSqlDatabase -Exactly 1 -Scope It
     }
 
     It "Should add database to elastic pool if one is specified" {
         $params['ElasticPool'] = "mon-foo-bar-epl"
 
-        .\New-DatabaseFromBlobFile @params
+        .\..\..\PSScripts\New-DatabaseFromBlobFile @params
 
-        Assert-MockCalled New-AzureRmSqlDatabaseImport -Exactly 1 -Scope It
-        Assert-MockCalled Get-AzureRmSqlDatabaseImportExportStatus -Exactly 1 -Scope It
-        Assert-MockCalled Set-AzureRmSqlDatabase -Exactly 1 -Scope It
-        Assert-MockCalled Get-AzureRmSqlDatabase -Exactly 0 -Scope It
+        Should -Invoke  New-AzureRmSqlDatabaseImport -Exactly 1 -Scope It
+        Should -Invoke  Get-AzureRmSqlDatabaseImportExportStatus -Exactly 1 -Scope It
+        Should -Invoke  Set-AzureRmSqlDatabase -Exactly 1 -Scope It
+        Should -Invoke  Get-AzureRmSqlDatabase -Exactly 0 -Scope It
     }
 
 }
